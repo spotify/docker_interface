2546c519494847e6697d0c24df8a8e15487780e8
Till Hoffmann <till@spotify.com>
2018-02-01 16:46:34 +0000

    Add license information.

--- docker_interface/docker_interface.py
+++ docker_interface/docker_interface.py
@@ -1,3 +1,17 @@
+# Copyright 2018 Spotify AB
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 
 
@@ -53,7 +67,7 @@ def build_docker_run_command(configuration):
     # Add the mounts
     for mount in run.pop('mount', []):
        if mount['type'] == 'tmpfs':
-           raise RuntimeError('tmpfs-mounts are currently not supported via the mount ' + 
+           raise RuntimeError('tmpfs-mounts are currently not supported via the mount ' +
                               'directive in docker_interface. Consider using the tmpfs ' +
                               'directive instead.' )
        if mount['type'] == 'bind':
