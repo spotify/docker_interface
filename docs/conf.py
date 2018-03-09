a2dde58911122269d60afaf33311520b034fdfdb
Till Hoffmann <till@spotify.com>
2018-03-09 12:31:39 +0000

    Run auto-generation of docs in subprocess.

--- docs/conf.py
+++ docs/conf.py
@@ -177,3 +177,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+import subprocess
+subprocess.check_call(['python', 'autodoc.py'])
